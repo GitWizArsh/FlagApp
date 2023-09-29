169ee2bdda6e127fe79a83d3bf5546e8b2fb7867
Arsh Singh <146023109+GitWizArsh@users.noreply.github.com>
2023-09-28 20:51:12 -0400

    Add files via upload

---
+++ FlagApplication/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace FlagApplication
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Infolabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FlagApplication.Properties.Resources.Twitter;
+            this.pictureBox1.Location = new System.Drawing.Point(68, 144);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 186);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::FlagApplication.Properties.Resources.eBook;
+            this.pictureBox2.Location = new System.Drawing.Point(278, 144);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(213, 186);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::FlagApplication.Properties.Resources.WEBINAR;
+            this.pictureBox3.Location = new System.Drawing.Point(507, 144);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(203, 186);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 15);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Click any image to see information";
+            // 
+            // Infolabel
+            // 
+            this.Infolabel.AutoSize = true;
+            this.Infolabel.Location = new System.Drawing.Point(347, 367);
+            this.Infolabel.Name = "Infolabel";
+            this.Infolabel.Size = new System.Drawing.Size(28, 15);
+            this.Infolabel.TabIndex = 4;
+            this.Infolabel.Text = "Info";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 520);
+            this.Controls.Add(this.Infolabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Img_App";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Infolabel;
+    }
+}
+
---
+++ FlagApplication/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlagApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Infolabel.Text = "Twitter, Inc. is an American communications company based in San Francisco, California." +
+                " The company operates the microblogging and social networking service Twitter. " +
+                "It previously operated the Vine short video app and Periscope livestreaming service.";
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Infolabel.Text = "An ebook is a book presented in a format that allows you to read it on an electronic device like a computer or handheld device. Several books that are available in printed versions can be found as ebooks.These include everything from bestselling fiction, classics to reference and academic texts.";
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            Infolabel.Text = "A conference call is a telephone call in which someone talks to 4 or more people at the same time. The conference call may be designed to allow the called party to participate during the call or set up so that the called party merely listens into the call and cannot speak.";
+        }
+    }
+}
